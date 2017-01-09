c365c91d8efc045aa2b6daa91c101953dce3d810
Jasmin Hudacsek <jasmin@hudacsek.com>
2017-01-09 10:46:43 -0700

    Add shoulda matchers config to rails helper because reasons

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should validate_presence_of :email }
-  it { should validate_uniqueness_of :email }
-  it { should have_many :links }
+  it { is_expected.to validate_presence_of :email }
+
+  it { is_expected.to have_many :links }
 end
